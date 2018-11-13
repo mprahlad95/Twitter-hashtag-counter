76b4d08da7107598380b639003a34acceaca722a
Prahlad Misra <mprahlad95@gmail.com>
2018-11-13 11:46:06 -0500

    abstract heap

--- src/FibonacciHeap.java
+++ src/FibonacciHeap.java
@@ -1,4 +1,39 @@
 
 public class FibonacciHeap {
 
+	// Insert a new node in the heap
+	public void insert() {
+
+	}
+
+	// Perform a cut; x from y
+	public void cut(Node x, Node y) {
+
+	}
+
+	// Perform cascading cut on the given node
+	public void cascadingCut(Node x) {
+
+	}
+
+	// Increase value of key for the input node
+	public void increaseKey(Node x, int k) {
+
+	}
+
+	// Extract max from the heap
+	public Node extractMax() {
+		return null;
+	}
+
+	// Perform degree-wise merge
+	public void pairWiseCombine() {
+
+	}
+
+	// Make y the child of node x
+	public void makeChild(Node y, Node x) {
+
+	}
+
 }
